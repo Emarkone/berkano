c7acf1e10d839084360593c9c928e4b88e688a57
EMarkOne <leo.claude@yahoo.com>
2021-09-16 10:53:02 +0200

    force real ip

--- app/Http/Controllers/TrackerController.php
+++ app/Http/Controllers/TrackerController.php
@@ -170,18 +170,20 @@ class TrackerController extends Controller
 
     protected function getIp()
     {
-        if(request()->ip == '192.168.1.1') return file_get_contents("http://ipecho.net/plain");
+        
 
         foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key) {
             if (array_key_exists($key, $_SERVER) === true) {
                 foreach (explode(',', $_SERVER[$key]) as $ip) {
                     $ip = trim($ip); // just to be safe
                     if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false) {
+                        if(request()->ip == '192.168.1.1') return file_get_contents("http://ipecho.net/plain");
                         return $ip;
                     }
                 }
             }
         }
+        if(request()->ip == '192.168.1.1') return file_get_contents("http://ipecho.net/plain");
         return request()->ip(); // it will return server ip when no client ip found
     }
 }
